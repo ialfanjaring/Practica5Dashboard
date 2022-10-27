dc8752c18cff84896fcfbb019a0395413860362f
ialfanjaring <ialfanjaring@iesch.org>
2022-10-27 11:43:04 +0200

    Insertar adecuados

---
+++ app/src/main/java/com/iesch/alfanjarin/ivan/practica5dashboard/MainActivity.java
@@ -0,0 +1,14 @@
+package com.iesch.alfanjarin.ivan.practica5dashboard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
